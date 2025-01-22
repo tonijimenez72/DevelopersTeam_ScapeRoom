45d7087d153eefaa1eadf3d4a036e32039e6a161
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2025-01-22 12:21:55 +0100

    Add models

--- src/main/java/service/RoomService.java
+++ src/main/java/service/RoomService.java
@@ -1,4 +1,10 @@
 package service;
 
+import model.Clue;
+import model.Decoration;
+import model.Room;
+
 public interface RoomService {
+    void addClue(Room room, Clue clue);
+    void addDecoration(Room room, Decoration decoration);
 }
